7db7db3d340c999c1e4bfdff0b092c7469d6aa74
Alexey Kharitonov <senator220291@gmail.com>
2020-12-23 12:00:29 +0300

    fix bugs

--- src/main/java/com/epam/esm/validator/GiftEntityValidator.java
+++ src/main/java/com/epam/esm/validator/GiftEntityValidator.java
@@ -2,8 +2,8 @@ package com.epam.esm.validator;
 
 public class GiftEntityValidator {
     private static final String ID_REGEX = "^[1-9]\\d{0,18}$";
-    private static final String NAME_REGEX = "\\p{L}{1,50}";
-    private static final String CERTIFICATE_DESCRIPTION_REGEX = "\\p{L}{1,250}";
+    private static final String NAME_REGEX = "^.{1,50}$";
+    private static final String CERTIFICATE_DESCRIPTION_REGEX = "^.{1,250}$";
 
     public static boolean correctId(String id) {
         return id.matches(ID_REGEX);
