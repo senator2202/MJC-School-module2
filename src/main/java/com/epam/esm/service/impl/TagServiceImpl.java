9116f7501aecd0a0a65c619f9e570246efb1a8f2
Alexey Kharitonov <senator220291@gmail.com>
2021-01-05 21:16:52 +0300

    add tests

--- src/main/java/com/epam/esm/service/impl/TagServiceImpl.java
+++ src/main/java/com/epam/esm/service/impl/TagServiceImpl.java
@@ -7,8 +7,6 @@ import com.epam.esm.service.TagService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.PlatformTransactionManager;
-import org.springframework.transaction.TransactionStatus;
-import org.springframework.transaction.support.TransactionCallbackWithoutResult;
 import org.springframework.transaction.support.TransactionTemplate;
 
 import java.util.List;
